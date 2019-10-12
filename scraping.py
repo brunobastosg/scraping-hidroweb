7b2d5c8fc2901935b9948e354fabcd6e66c5217e
Bruno Guimarães <brunobastosg@gmail.com>
2019-10-12 00:40:12 -0300

    Refatorando código para maior legibilidade e velocidade

--- scraping.py
+++ scraping.py
@@ -1,11 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium
 import os
 import time
 
 URL_TO_SCRAPE = 'http://www.snirh.gov.br/hidroweb/publico/medicoes_historicas_abas.jsf'
 
-chromedriver_path = os.environ.get('CHROMEDRIVER')
-driver = webdriver.Chrome(chromedriver_path)
+SHORT_TIMEOUT  = 5
+LONG_TIMEOUT = 30
+LOADING_ELEMENT_XPATH = '//div[@class="loading"]'
+
+ITENS_POR_PAGINA = 5
+ULTIMA_PAGINA = 7238
+
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER')
+
+driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+
+def abrir_site():
+    driver.get(URL_TO_SCRAPE)
 
 def clicar_botao_consultar():
     obter_botao_consultar().click()
@@ -13,38 +28,59 @@ def clicar_botao_consultar():
 def obter_botao_consultar():
     return driver.find_element_by_id('form:fsListaEstacoes:bt')
 
-def selecionar_checkboxes():
-    for j in range(0, 5):
-        checkbox = driver.find_elements_by_css_selector('input[type="checkbox"]')[j]
-        checkbox.send_keys(webdriver.common.keys.Keys.SPACE)
-        time.sleep(2)
-        driver.find_elements_by_css_selector('input[type="radio"]')[2].send_keys(webdriver.common.keys.Keys.SPACE)
-        time.sleep(2)
-        driver.find_element_by_id('form:fsListaEstacoes:fsListaEstacoesC:btBaixar').click()
-        time.sleep(2)
+def selecionar_estacoes():
+    for i in range(0, ITENS_POR_PAGINA):
+        selecionar_estacao(i)
+        aguardar_loading()
+        baixar_arquivo()
+
+def selecionar_estacao(indice):
+    driver.find_elements_by_css_selector('input[type="checkbox"]')[indice].send_keys(webdriver.common.keys.Keys.SPACE)
 
 def exibir_pagina(numeroPagina):
     driver.find_element_by_link_text(str(numeroPagina)).click()
 
+def baixar_arquivo():
+    driver.find_element_by_id('form:fsListaEstacoes:fsListaEstacoesC:btBaixar').click()
+
+def selecionar_tipo_arquivo_csv():
+    driver.find_elements_by_css_selector('input[type="radio"]')[2].send_keys(webdriver.common.keys.Keys.SPACE)
+
+def aguardar_loading():
+    try:
+        WebDriverWait(driver, SHORT_TIMEOUT).until(EC.visibility_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
+    except selenium.common.exceptions.TimeoutException:
+        pass
+
+    try:
+        WebDriverWait(driver, LONG_TIMEOUT).until_not(EC.visibility_of_element_located((By.XPATH, LOADING_ELEMENT_XPATH)))
+    except selenium.common.exceptions.TimeoutException:
+        pass
+
+def clicar_aba_dados_convencionais():
+    driver.find_element_by_css_selector('a[href="#dadosConvencionais"]').click()
+
+def processar_pagina(indice_proxima_pagina):
+    selecionar_estacoes()
+    exibir_pagina(indice_proxima_pagina)
+    return indice_proxima_pagina + 1
+
 def scrape():
-    driver.get(URL_TO_SCRAPE)
+    abrir_site()
 
     clicar_botao_consultar()
 
-    time.sleep(5)
+    aguardar_loading()
 
-    driver.find_element_by_css_selector('a[href="#dadosConvencionais"]').click()
+    clicar_aba_dados_convencionais()
 
-    ultima_pagina = 7238
-    i = 2
+    selecionar_tipo_arquivo_csv()
 
-    selecionar_checkboxes()
-    exibir_pagina(i)
-    i = i + 1
+    aguardar_loading()
 
-    while i < ultima_pagina:
-        selecionar_checkboxes()
-        exibir_pagina(i)
-        i = i + 1
+    i = processar_pagina(2)
+
+    while i < ULTIMA_PAGINA:
+        i = processar_pagina(i)
 
 scrape()
