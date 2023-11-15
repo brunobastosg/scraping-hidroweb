5c003dabe464f53e380ed0bf36c083bfa2526d97
Bruno Bastos Guimaraes <bruno.guimaraes@serpro.gov.br>
2023-11-15 17:05:49 -0300

    Obtendo estações telemétricas do serviço SOAP

--- obter_estacoes_telemetricas.py
+++ obter_estacoes_telemetricas.py
@@ -1,35 +1,19 @@
-import json
-import requests
+import simplejson as json
+import zeep
 
-TAMANHO_PAGINA = 100
-URL = 'http://www.snirh.gov.br/hidroweb/rest/api/estacaotelemetrica'
+WSDL = 'https://telemetriaws1.ana.gov.br/ServiceANA.asmx?wsdl'
 
-pagina_atual = 0
+client = zeep.Client(wsdl=WSDL)
+result = client.service.ListaEstacoesTelemetricas(statusEstacoes='', origem='')
 
-total_paginas = None
+estacoes = []
 
-dados = []
-
-response = requests.get(URL + '?size={}&page={}'.format(TAMANHO_PAGINA, pagina_atual))
-
-if response.status_code == 200:
-    retorno = json.loads(response.content.decode('utf-8'))
-    total_paginas = retorno['totalPages']
-else:
-    raise Exception('Erro ao obter total de páginas dos dados de estações telemétricas')
+for item in result._value_1._value_1:
+  estacao = {}
+  for key in item['Table']:
+     estacao[key] = item['Table'][key]
+  estacoes.append(estacao)
 
 with open('ids_estacoes_telemetricas.txt', 'a') as ids_file:
-    while pagina_atual <= total_paginas:
-        print('Obtendo página {} de {}...'.format(pagina_atual, total_paginas))
-        response = requests.get(URL + '?size={}&page={}'.format(TAMANHO_PAGINA, pagina_atual))
-        if response.status_code == 200:
-            retorno = json.loads(response.content.decode('utf-8'))
-            dados += retorno['content']
-            for estacao in retorno['content']:
-                ids_file.write(str(estacao['id']) + '\n')
-            pagina_atual = pagina_atual + 1
-        else:
-            raise Exception('Erro ao obter a página {} dos dados de estações telemétricas'.format(pagina_atual))
-
-with open('estacoes_telemetricas.json', 'w') as json_file:
-     json.dump(dados, json_file)
+  for estacao in estacoes:
+    ids_file.write(str(estacao['CodEstacao']) + '\n')
